6e0b21fd808e85de130eaabcdc0051f55f8221ed
Arjun Singh <57751277+arjun07singh@users.noreply.github.com>
2021-06-14 18:07:45 +0530

    Add files via upload

---
+++ Java/Arjun Singh Sandhu - 1922012/name.java
@@ -0,0 +1,14 @@
+class Name
+{
+static String name ="Arjun";
+static int rollno=1922012;
+static void display( )
+{
+System.out.println("Name: "+ name);
+System.out.println("Rollno: " + rollno);
+}
+public static void main(String args[] )
+{
+display ( );
+}
+} 
